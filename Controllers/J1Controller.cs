ad260dd5f24ce387c755d4a820a46d475f9c3a3c
Samson Mathews <samsonmmathews@gmail.com>
2025-06-18 14:29:39 -0400

    Question 1 done

--- Controllers/J1Controller.cs
+++ Controllers/J1Controller.cs
@@ -7,5 +7,24 @@ namespace Backend_Assignment_02.Controllers
     [ApiController]
     public class J1Controller : ControllerBase
     {
+        /// <summary>
+        /// Based on the number of deliveries and collisions, we will calculate the score for the robot
+        /// </summary>
+        /// <param name="Collisions">The number of collisions that happened</param>
+        /// <param name="Deliveries">The number of packages delivered</param>
+        /// <returns>
+        /// Returns the total score after calculating the number of points
+        /// </returns>
+        [HttpPost(template: "Delivedroid")]
+        public int Delivedroid([FromForm] int Collisions, [FromForm] int Deliveries)
+        {
+            int score = ((Deliveries * 50) - (Collisions * 10));
+
+            if (Deliveries > Collisions)
+            {
+                score += 500;
+            }
+            return score;
+        }
     }
 }
